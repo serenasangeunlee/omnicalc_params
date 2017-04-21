9b76624dc58b63afe6825334e63072291085d1cd
serenasangeunlee <serena.lee@kellogg.northwestern.edu>
2017-04-21 10:30:36 -0500

    update

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,8 @@
+class CalculationsController < ApplicationController
+
+  def flex_square
+
+    render("calculations/flex_square.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/flexible/square/5", { :controler => "calculations", :action => "flex_square" })
+
 end
