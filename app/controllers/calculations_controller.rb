308e9def7d10ad3f243ee1294e31900bd69741b9
serenasangeunlee <serena.lee@kellogg.northwestern.edu>
2017-04-21 13:41:10 -0500

    update

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,8 +1,48 @@
 class CalculationsController < ApplicationController
 
   def flex_square
-
+    # params = ["the_number"=>"25"]
+    @user_provided_number = params["the_number"].to_f
+    @squared_number = @user_provided_number ** 2
     render("calculations/flex_square.html.erb")
+  end
+
+  def flex_square_root
+    # params = ["the_number"=>"25"]
+    @user_provided_number = params["the_number"].to_f
+    @square_rooted_number = @user_provided_number ** 0.5
+    render("calculations/flex_square_root.html.erb")
+  end
+
+  def flex_payment
+    # params = ["the_number"=>"25"]
+    @user_provided_year = params["the_year"].to_f
+    @user_provided_loan = params["the_loan"].to_f
+    @user_provided_rate = params["the_rate"].to_f
+    @monthly_payment = ((@user_provided_rate/ 100 / 12) * @user_provided_loan) / (1 - ((1 + (@user_provided_rate / 100 / 12)) ** (-@user_provided_year * 12)))
+    render("calculations/flex_payment.html.erb")
+  end
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
+  def square
+    # params = ["number_to_be_squared"=>"42"]
+    @user_provided_number = params["number_to_be_squared"].to_f
+    @squared_number = @user_provided_number ** 2
+    render("calculations/square.html.erb")
+  end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    # params = ["number_to_be_squared"=>"42"]
+    @user_provided_number = params["number_to_be_squared"].to_f
+    @square_rooted_number = @user_provided_number ** 0.5
+    render("calculations/square_root.html.erb")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get("/flexible/square/5", { :controler => "calculations", :action => "flex_square" })
+  get("/flexible/square/:the_number", { :controller => "calculations", :action => "flex_square" })
+  get("/flexible/square_root/:the_number", { :controller => "calculations", :action => "flex_square_root" })
+  get("/flexible/payment/:the_year/:the_loan/:the_rate", { :controller => "calculations", :action => "flex_payment" })
+
+  get("/square/new", { :controller => "calculations", :action => "square_form" })
+  get("/square/results", { :controller => "calculations", :action => "square" })
+
+  get("/square_root/new", { :controller => "calculations", :action => "square_root_form" })
+  get("/square_root/results", { :controller => "calculations", :action => "square_root" })
 
 end
