dd507497196916e4a96b18ff7a1dd60c2e6f1c34
serenasangeunlee <serena.lee@kellogg.northwestern.edu>
2017-04-27 18:38:14 -0500

    update

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,21 +1,18 @@
 class CalculationsController < ApplicationController
 
   def flex_square
-    # params = ["the_number"=>"25"]
     @user_provided_number = params["the_number"].to_i
     @squared_number = @user_provided_number ** 2
     render("calculations/flex_square.html.erb")
   end
 
   def flex_square_root
-    # params = ["the_number"=>"25"]
     @user_provided_number = params["the_number"].to_i
     @square_rooted_number = @user_provided_number ** 0.5
     render("calculations/flex_square_root.html.erb")
   end
 
   def flex_payment
-    # params = ["the_number"=>"25"]
     @basis_points = params["basis_points"].to_f
     @number_of_years = params["number_of_years"].to_i
     @present_value = params["present_value"].to_i
@@ -39,7 +36,7 @@ class CalculationsController < ApplicationController
 
   def square
     # params = ["number_to_be_squared"=>"42"]
-    @user_provided_number = params["number_to_be_squared"].to_f
+    @user_provided_number = params["number_to_be_squared"].to_i
     @squared_number = @user_provided_number ** 2
     render("calculations/square.html.erb")
   end
@@ -49,10 +46,54 @@ class CalculationsController < ApplicationController
   end
 
   def square_root
-    # params = ["number_to_be_squared"=>"42"]
-    @user_provided_number = params["number_to_be_squared"].to_f
+    @user_provided_number = params["number_to_be_squared"].to_i
     @square_rooted_number = @user_provided_number ** 0.5
     render("calculations/square_root.html.erb")
+  end
+
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
+
+  def payment
+    @basis_points = params["basis_points"].to_f
+    @number_of_years = params["number_of_years"].to_i
+    @present_value = params["present_value"].to_i
+    @monthly_payment = ((@basis_points / 100 / 12) * @present_value) / (1 - ((1 + (@basis_points / 100 / 12)) ** (-@number_of_years * 12)))
+    render("calculations/payment.html.erb")
+  end
+
+  def random_form
+    render("calculations/random_form.html.erb")
+  end
+
+  def random
+    @user_provided_min = params["min"].to_i
+    @user_provided_max = params["max"].to_i
+    @random_number = my_rand(@user_provided_max, @user_provided_min)
+    render("calculations/random.html.erb")
+  end
+
+  def word_count_form
+    render("calculations/word_count_form.html.erb")
+  end
+
+  def word_count
+    @text = params["text"]
+    @special_word = params["special_word"]
+    @word_count = @text.split.count
+    @character_count_with_spaces = @text.length - @text.count("\r")
+    @character_count_without_spaces = @text.gsub(" ","").length - @text.count("\n") - @text.count("\r")
+    @occurrences = @text.downcase.scan(@special_word.downcase).count
+    render("calculations/word_count.html.erb")
+  end
+
+  def descriptive_strats_form
+    render("calculations/descriptive_strats_form.html.erb")
+  end
+
+  def descriptive_strats
+    render("calculations/descriptive_strats.html.erb")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,16 @@ Rails.application.routes.draw do
   get("/square_root/new", { :controller => "calculations", :action => "square_root_form" })
   get("/square_root/results", { :controller => "calculations", :action => "square_root" })
 
+  get("/payment/new", { :controller => "calculations", :action => "payment_form" })
+  get("/payment/results", { :controller => "calculations", :action => "payment" })
+
+  get("/random/new", { :controller => "calculations", :action => "random_form" })
+  get("/random/results", { :controller => "calculations", :action => "random" })
+
+  get("/word_count/new", { :controller => "calculations", :action => "word_count_form" })
+  get("/word_count/results", { :controller => "calculations", :action => "word_count" })
+
+  get("/descriptive_strats/new", { :controller => "calculations", :action => "descriptive_strats_form" })
+  get("/descriptive_strats/results", { :controller => "calculations", :action => "descriptive_strats" })
+
 end
